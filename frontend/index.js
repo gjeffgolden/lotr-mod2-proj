b08183227e4663790442677ff5ae53ac579a9208
gjeffgolden <gjgolden08@gmail.com>
2021-01-25 14:21:37 -0700

    Front end first steps.

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+let charArray = []
+
+fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(characters => {
+        characters.forEach(character => {
+            const li = document.createElement('li')
+            li.innerHTML = `
+            <a href="${character.image}" target="_blank">${character.name}</a>
+            `
+            document.querySelector('#character-list').append(li)
+        })
+    })
+
+
