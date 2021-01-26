347aff7ffa832178ba62a9c12248747e40f1cb41
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-01-26 14:51:26 -0700

    trouble with filtering

---
+++ frontend/allCharacters.js
@@ -0,0 +1,42 @@
+let raceArray = []
+
+const backendBaseURL = 'http://localhost:3000'
+let charactersURL = `${backendBaseURL}/characters`
+
+const filterParams = new URLSearchParams(window.location.search)
+const charRace = filterParams.get('race')
+
+console.log(charRace)
+if (charRace) {
+    charactersURL = `${charactersURL}?race=${charRace}`
+}
+
+
+fetch(charactersURL)
+.then(response => response.json())
+.then(characters => {
+  characters.forEach(character => {
+    if(!raceArray.includes(character.race)){
+      raceArray.push(character.race)
+      const option = document.createElement('option')
+      
+      // const li = document.createElement
+      option.textContent = character.race
+      option.value = character.race
+      
+      document.querySelector('#race-dropdown').append(option)
+    }
+  })
+})
+
+fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(characters => {
+        characters.forEach(character => {
+            const li = document.createElement('li')
+            li.innerHTML = `
+            <a href="/characters.html?id=${character.id}">${character.name}</a>
+            `
+            document.querySelector('#filtered-characters').append(li)
+        })
+    })
